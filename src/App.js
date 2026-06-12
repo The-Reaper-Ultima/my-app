0ffcbf94b9ad2935d752d8352d03fc90381dc897
Rahul Udani <rahuludani1@gmail.com>
2026-06-12 14:42:57 -0700

    Update logo to Propeller Aero and link to DirtMate

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://propelleraero.com" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://propelleraero.com"
           target="_blank"
           rel="noopener noreferrer"
         >
